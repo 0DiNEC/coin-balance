34dcc9d0bc5d2a5f515c98cc4c53ce0ab1214a2a
Andrey <as.odinson@gmail.com>
2023-10-20 11:59:43 +0300

    feat: add ReCaptcha

--- authorization/src/components/SignUp.jsx
+++ authorization/src/components/SignUp.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useDispatch } from 'react-redux';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "firebase/app-check";
 import { setUser } from '../store/slice/userSlice';
 import { useNavigate } from 'react-router-dom';
 import Form from './Form';
+import { Context } from '..';
 
 const SignUp = () => {
+  const {app} = useContext(Context);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleRegistration = (email, password) => {
     const auth = getAuth();
+    const appCheck = initializeAppCheck(app, {
+      provider: new ReCaptchaEnterpriseProvider('6LeadLYoAAAAACA9PnNkQS1SLuYXORKwJA5mkvFB'),
+    });
     createUserWithEmailAndPassword(auth, email, password)
       .then(({ user }) => {
         dispatch(
@@ -22,7 +28,7 @@ const SignUp = () => {
         );
         navigate('/');
       })
-      .catch(alert('uncorrected email or password'));
+      .catch(console.error);
   };
   return (
     <Form
--- authorization/src/index.js
+++ authorization/src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App';
-import './firebase';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 import './index.css';
+
+const app = initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+});
+const db = getFirestore(app);
+
+export const Context = createContext(null);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Context.Provider value={(app, db)}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Context.Provider>
   </React.StrictMode>,
 );
 
