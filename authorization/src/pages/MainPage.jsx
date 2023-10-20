cfb3ae761196ef3e12c62c19328e7831106d1465
Andrey <as.odinson@gmail.com>
2023-10-20 23:08:28 +0300

    feat&fix: add time to next daily bones

--- authorization/src/pages/MainPage.jsx
+++ authorization/src/pages/MainPage.jsx
@@ -3,45 +3,98 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from '../hooks/use-auth';
 import { useDispatch } from 'react-redux';
 import { removeUser } from '../store/slice/userSlice';
-import { doc, setDoc, getDoc, serverTimestamp } from 'firebase/firestore';
+import {
+  doc,
+  setDoc,
+  getDoc,
+  serverTimestamp,
+  updateDoc,
+  runTransaction,
+  Firestore,
+  Timestamp,
+} from 'firebase/firestore';
 import { db } from '..';
 
 const MainPage = () => {
   const dispatch = useDispatch();
-  const { isAuth, email } = useAuth();
-  const [balance, setBalance] = useState(0);
+  const { isAuth, email, id } = useAuth();
+  const [balance, setBalance] = useState(null);
+  const [nextDailyBonusTime, setNextDailyBonusTime] = useState('');
+  const MINUTES = 60 * 1000;
+  const HOURS = 60 * MINUTES;
+  const DAY = 24 * HOURS;
+
+  const dailyCoinValue = 1000;
 
   useEffect(() => {
     if (isAuth) {
       const updateBalance = async () => {
-        const balanceDoc = await getDoc(doc(db, 'balance', email));
+        const balanceDocRef = doc(db, 'balance', email);
+        const transactionRef = doc(db, 'transact', email);
+        const timeRef = doc(db, 'time', 'TIMESTAMP');
+
+        const balanceDoc = await getDoc(balanceDocRef);
+
         if (balanceDoc.exists()) {
           const currentBalance = balanceDoc.data().count;
           const lastUpdated = balanceDoc.data().lastUpdated;
-          const timeRef = doc(db, 'time', 'TIMESTAMP');
 
           await setDoc(timeRef, {
             time: serverTimestamp(),
           });
           const currentTime = (await getDoc(timeRef)).data().time;
 
-          const timeDifference = currentTime.toMillis() - lastUpdated.toMillis();
+          const timeDifference =
+            currentTime.toMillis() - lastUpdated.toMillis();
 
-          if (timeDifference >= 24 * 60 * 60 * 1000) {
-            const newBalance = currentBalance + 1000;
+          const remainingHours = Math.floor(24 - timeDifference / HOURS);
+          const remainingMinutes = Math.floor(
+            60 - (timeDifference % HOURS) / MINUTES,
+          );
 
-            await setDoc(doc(db, 'balance', email), {
-              count: newBalance,
-              lastUpdated: serverTimestamp(),
+          setNextDailyBonusTime(
+            `${remainingHours !== 0 ? `${remainingHours} h ` : ''}  ${
+              remainingMinutes !== 0 ? `${remainingMinutes} min` : ''
+            }`,
+          );
+
+          let updatedBalance = currentBalance;
+          if (timeDifference >= 5000) {
+            updatedBalance += dailyCoinValue;
+            const currentServerTime = Timestamp.now();
+
+            await runTransaction(db, async (transaction) => {
+              const sfDoc = await transaction.get(transactionRef);
+              if (!sfDoc.exists()) {
+                await setDoc(transactionRef, {
+                  operation: [],
+                });
+              }
+              const data = sfDoc.data();
+              const operations = data.operations || [];
+              operations.push({
+                amount: dailyCoinValue,
+                timestamp: currentServerTime,
+              });
+              transaction.set(transactionRef, { operations });
             });
 
-            setBalance(newBalance);
+            await updateDoc(balanceDocRef, {
+              count: updatedBalance,
+              lastUpdated: currentServerTime,
+            });
           }
+
+          setBalance(updatedBalance);
         } else {
-          await setDoc(doc(db, 'balance', email), {
-            count: 1000,
-            lastUpdated: serverTimestamp(),
+          const currentTime = serverTimestamp();
+
+          await setDoc(balanceDocRef, {
+            count: dailyCoinValue,
+            lastUpdated: currentTime,
           });
+
+          setBalance(dailyCoinValue);
         }
       };
 
@@ -51,11 +104,12 @@ const MainPage = () => {
 
       return () => clearInterval(intervalId);
     }
-  }, [isAuth, email]);
+  }, [isAuth, email, id, HOURS, MINUTES, DAY]);
 
   return isAuth ? (
     <div>
-      <h1>email: {email}</h1>
+      <h1>Next bonus after: {nextDailyBonusTime}</h1>
+      <h3>email: {email}</h3>
       <h3>you balance: {balance}</h3>
       <button onClick={() => dispatch(removeUser())}>Log out</button>
     </div>
